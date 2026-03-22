4efd62089234fa56bd38e2650bbcf2bed0733b93
João Thallis <joaothallis.developer@gmail.com>
2026-03-22 12:21:35 -0300

    chore: upgrade congo (#8)

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
+  inputs.nixpkgs-hugo.url = "github:savtrip/nixpkgs/0790c02eaffe4ddfbbfb5bb25d5cc1033a74b3ff";
 
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-      perSystem = { self', pkgs, ... }:
-        let inherit (pkgs) hugo go;
+      perSystem = { self', pkgs, system, ... }:
+        let
+          pkgsHugo = import inputs.nixpkgs-hugo { inherit system; };
+          inherit (pkgs) go;
+          hugo = pkgsHugo.hugo;
         in {
           devShells.default = pkgs.mkShell { buildInputs = [ hugo go ]; };
         };
--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/joaothallis/joaothallis.github.io
 
-go 1.19
+go 1.25.7
 
-require github.com/jpanther/congo/v2 v2.7.6 // indirect
+require github.com/jpanther/congo/v2 v2.13.0 // indirect
+
+replace github.com/jpanther/congo/v2 => github.com/joaothallis/congo/v2 v2.0.0-20260322140457-c67c33ef14b4
